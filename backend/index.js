c949c14b3e86a8b8a4e3be9ad3ccaf30df816b0f
Pranay <pranaypandeyofficial@gmail.com>
2023-12-28 16:24:21 +0530

    bodyparser

--- backend/index.js
+++ backend/index.js
@@ -4,8 +4,10 @@ require("dotenv").config();
 const port = process.env.PORT || 4001;
 const router = require("./router/index.js");
 const cors = require("cors");
+var bodyParser = require('body-parser')
 
-app.use(express.json());
+/*app.use(express.json());*/
+app.use(bodyParser.json()); app.use(bodyParser.urlencoded({ extended: true }));
 
 const corsOptions = {
   origin: "*", // Replace this with the actual frontend origin(s) you want to allow
